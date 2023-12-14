881942a7f15abd074f4ca07171734da6af76cf66
KANG0417 <ipia99@naver.com>
2023-12-14 15:57:21 +0900

    [level1] 경로 수정

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,5 +1,5 @@
-import Footer from "layout/Footer";
-import Header from "layout/Header";
+import Footer from "components/layout/Footer";
+import Header from "components/layout/Header";
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
